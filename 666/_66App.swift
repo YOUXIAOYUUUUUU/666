d280ab241fcbd22660d8f68625b33c48e9d7dc5f
XIAOYU YOU <youxiaoyu1117@gmail.com>
2024-11-04 15:18:58 +0100

    Initial Commit

---
+++ 666/_66App.swift
@@ -0,0 +1,17 @@
+//
+//  _66App.swift
+//  666
+//
+//  Created by XIAOYU YOU on 04/11/24.
+//
+
+import SwiftUI
+
+@main
+struct _66App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
